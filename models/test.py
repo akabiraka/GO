af78402fa0eb7bd1f4dd53dd297af37122dad90e
akabiraka <akabir0101@gmail.com>
2022-08-13 11:12:09 -0400

    test updated

--- models/test.py
+++ models/test.py
@@ -10,18 +10,19 @@ from models.Dataset_1 import SeqAssociationDataset, TermsGraph, get_class_weight
 import models.MultimodalTransformer as MultimodalTransformer
 
 import utils as Utils
+from val_1 import run_val
+from test_1 import run_test
 
 config = Config()
 out_filename = config.get_model_name()
-out_filename = out_filename+"_perf" #_loss
+out_filename = out_filename+"_loss" #
 print(f"Running test: {out_filename}")
 
 
-# loading model, criterion, optimizer, summarywriter
+# loading model, criterion
 model = MultimodalTransformer.Model(config=config).to(config.device)
 class_weights = get_class_weights(config.species, config.GO).to(config.device)
-# pos_class_weights = get_positive_class_weights(config.species, config.GO).to(config.device)
-criterion = torch.nn.BCEWithLogitsLoss(class_weights)
+label_pred_criterion = torch.nn.BCEWithLogitsLoss(class_weights)
 
 # loading learned weights
 checkpoint = torch.load(f"outputs/models/{out_filename}.pth")
@@ -30,10 +31,7 @@ model.load_state_dict(checkpoint['model_state_dict'])
 
 # loading dataset
 terms_graph = TermsGraph(config.species, config.GO, config.n_samples_from_pool)
-test_dataset = SeqAssociationDataset(config.species, config.GO, dataset="test")
-test_loader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False)
 
+val_loss, val_tmax, val_fmax, val_smin, val_aupr = run_val(model, terms_graph, label_pred_criterion)
+test_loss, test_tmax, test_fmax, test_smin, test_aupr = run_test(model, terms_graph, label_pred_criterion)
 
-test_loss, true_scores, pred_scores = MultimodalTransformer.val(model, test_loader, terms_graph, criterion, config.device)
-Utils.save_as_pickle(true_scores, f"outputs/predictions/{out_filename}_true_scores.pkl")
-Utils.save_as_pickle(pred_scores, f"outputs/predictions/{out_filename}_pred_scores.pkl")
